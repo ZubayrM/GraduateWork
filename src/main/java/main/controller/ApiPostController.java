dfc80fa24b4acac9181249f71fa84e82e9046903
ZubayrM <zubayr_@live.com>
2020-03-23 00:12:12 +0300

    no comment

--- src/main/java/main/controller/ApiPostController.java
+++ src/main/java/main/controller/ApiPostController.java
@@ -7,10 +7,8 @@ import main.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
 import java.util.*;
 
